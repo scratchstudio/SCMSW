b65c8deb785385ebb5892e9c0a18c7ef34f9c3a4
kjnam <kyujnam@gmail.com>
2017-11-03 11:02:47 +0900

    프로세스에 이름을 붙이고 액터가 응답을 보내는 Counter 예제

--- Actors/counter/counter.ex
+++ Actors/counter/counter.ex
@@ -1,19 +1,27 @@
 defmodule Counter do
   def start(count) do
-    spawn(__MODULE__, :loop, [count]) # 모듈이름, 함수이름, 함수에 전달될 인수
+    pid = spawn(__MODULE__, :loop, [count]) # 모듈이름, 함수이름, 함수에 전달될 인수
+    Process.register(pid, :counter) # 프로세스 식별자 대신 이름을 사용
+    pid
   end
-  def next(counter) do
-    send(counter, {:next})
+  def next do
+    ref = make_ref() # 고유의 참조 주소 생성
+    send(:counter, {:next, self(), ref})
+    receive do
+      {:ok, ^ref, count} -> count
+    end
   end
   def loop(count) do
     receive do
-      {:next} ->
-        IO.puts("Current count: #{count}")
+      {:next, sender, ref} ->
+        send(sender, {:ok, ref, count})
+        #IO.puts("Current count: #{count}")
         loop(count + 1)
     end
   end
 end
 
-counter = Counter.start(42)
-Counter.next(counter)
-Counter.next(counter)
+# iex counter.ex
+# Counter.start(42)
+# Counter.next(counter)
+# Counter.next(counter)
