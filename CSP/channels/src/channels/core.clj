1543bda4d5fb89bfd12a9aa215017f009c1defd0
iamkyu <kyujnam@gmail.com>
2017-11-10 12:13:14 +0900

    고블록을 통한 (비효율적인) 더하기 함수 예제

--- CSP/channels/src/channels/core.clj
+++ CSP/channels/src/channels/core.clj
@@ -2,3 +2,5 @@
   (:require [clojure.core.async :as async :refer :all
               :exclude [map into reduce merge partition partition-by take]]))
 
+(defn go-add [x y]
+  (<!! (nth (iterate #(go (inc (<! %))) (go x)) y)))
